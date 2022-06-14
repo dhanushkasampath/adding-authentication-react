0059be623fef0b47ef1afeeb60665575e427a991
dsampath <dsampath@wiley.com>
2022-06-14 21:17:28 +0530

    called a protected resource(firebase api of password change) using token got after login

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,11 +1,49 @@
+import {useRef, useContext} from "react";
+
 import classes from './ProfileForm.module.css';
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+  const newPasswordRef = useRef();
+
+  const authCtx = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+      event.preventDefault();
+      const enteredNewPassword = newPasswordRef.current.value;
+
+      //add validations
+      let url = 'https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyAJJS6NoP5zR1TFmJoS4IPz589ObIybWBo';
+      fetch(
+          url,
+          {
+              method: 'POST',
+              body: JSON.stringify({
+                  idToken: authCtx.token,
+                  password: enteredNewPassword,
+                  returnSecureToken: true
+              }),
+              headers: {
+                  'Content-Type': 'application/json'
+              }
+          }
+      ).then(res => {
+          if (res.ok) {
+              return res.json();
+          } else {
+              res.json().then(data => {
+                  let errorMessage = 'Password Change Failed!';
+                  throw new Error(errorMessage);
+              });
+          }
+      })
+  }
+
   return (
-    <form className={classes.form}>
+    <form onSubmit={submitHandler} className={classes.form}>
       <div className={classes.control}>
         <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <input type='password' id='new-password' minLength="7" ref={newPasswordRef} />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
