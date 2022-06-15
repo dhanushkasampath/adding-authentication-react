12242744508d0517d060d09ee664f7878d7d7540
dsampath <dsampath@wiley.com>
2022-06-15 08:45:40 +0530

    stored the token in local storage of the browser

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -9,16 +9,19 @@ const AuthContext = React.createContext({
 
 //below are called named export
 export const AuthContextProvider = (props) => {
-    const [token, setToken] = useState(null);
+    const initialToken = localStorage.getItem('token');//check the local storage when the app starts.
+    const [token, setToken] = useState(initialToken);
 
     const userIsLoggedIn = !!token; //this returns true if token is there.
 
     const loginHandler = (token) => {//set the token
         setToken(token);
+        localStorage.setItem('token', token);//store the token in local storage
     };
 
     const logoutHandler = () => {//remove the token
         setToken(null);
+        localStorage.removeItem('token');//remove the token from local storage when user log-out
     };
 
     const contextValue = {
