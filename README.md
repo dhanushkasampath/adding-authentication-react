f98affb8a8fb40638a92372d9a2eaec92ca4e2c7
dsampath <dsampath@wiley.com>
2022-06-16 09:22:07 +0530

    if user manually logged out clear the token stored.

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ https://firebase.google.com/docs/reference/rest/auth#section-sign-in-email-passw
 "expiresIn": "3600"
 }
 
+1. User should logout when he manually logged out.
+2. Then stored Token and expiration time should be cleared.
+
+3. When user logged in token and expirationTime should stored in the localstorage.
+4. When user refreshed the browser login state should not change.
+5. User should not able to render(enter url directly on browser) protected pages if not logged in.
+6. User should automatically logged out after the expiration time. expiration time is in the token and it is setted 
+   in code
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,6 @@
-import React, {useState} from "react";
+import React, {useState, useEffect, useCallback} from "react";
+//now we'll refine this timer approach if the user manually logged out.
+let logoutTimer;
 
 const AuthContext = React.createContext({
     token: '',
@@ -13,10 +15,34 @@ const calculateRemainingTime = (expirationTime) => {
     return adjExpirationTime - currentTime;
 }
 
+const retrieveStoredToken = () => {
+    const storedToken = localStorage.getItem('token');
+    const storedExpirationDate = localStorage.getItem('expirationTime');
+
+    const remainingTime = calculateRemainingTime(storedExpirationDate);
+
+    //if below if block does not execute we know we have more time.
+    if(remainingTime <= 60000){//1 minute
+        localStorage.removeItem('token');
+        localStorage.removeItem('expirationTime');
+        return null;
+    }
+
+    //this is how we return a token
+    return {
+        token: storedToken,
+        duration: remainingTime
+    }
+}
+
 
 //below are called named export
 export const AuthContextProvider = (props) => {
-    const initialToken = localStorage.getItem('token');//check the local storage when the app starts.
+    const tokenData = retrieveStoredToken();
+    let initialToken;
+    if(tokenData){//here we check token data is truthy.
+        initialToken = tokenData.token;
+    }
     const [token, setToken] = useState(initialToken);
 
     const userIsLoggedIn = !!token; //this returns true if token is there.
@@ -24,15 +50,32 @@ export const AuthContextProvider = (props) => {
     const loginHandler = (token, expirationTime) => {//set the token
         setToken(token);
         localStorage.setItem('token', token);//store the token in local storage
+        localStorage.setItem('expirationTime', expirationTime);//store the expirationTime in local storage
 
         const remainingTime = calculateRemainingTime(expirationTime);
-        setTimeout(logoutHandler, remainingTime);//you can test this by adding a hardcoded value as 3000 milliseconds
+        logoutTimer = setTimeout(logoutHandler, remainingTime);//you can test this by adding a hardcoded value as 3000
+        // milliseconds
+
     };
 
-    const logoutHandler = () => {//remove the token
+    //We should ensure that this function is not re-created unnecessarily to prevent
+    // infininte loops. For that we can use "CallBacks"
+    const logoutHandler = useCallback(() => {//remove the token
         setToken(null);
         localStorage.removeItem('token');//remove the token from local storage when user log-out
-    };
+        localStorage.removeItem('expirationTime');//remove the expirationTime from local storage when user log-out
+        if(logoutTimer){
+            clearTimeout(logoutTimer);
+        }
+    }, []);
+
+    //if tokenData changes this function execute
+    useEffect(()=>{
+        if(tokenData){
+            console.log(tokenData.duration);
+            logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+        }
+    },[tokenData, logoutHandler]);
 
     const contextValue = {
         token: token,
